c757eb12a9367e7889cfa442d146aeef6075f975
David A. <3106338+Dakad@users.noreply.github.com>
2025-02-10 10:26:08 +0100

    Generate Yard docs for the the GithubActionCheckRunFormatter class

--- src/github_action_check_run_formatter.rb
+++ src/github_action_check_run_formatter.rb
@@ -3,9 +3,35 @@ require_relative './annotation'
 
 module Pronto
   module Formatter
+    # The GithubActionCheckRunFormatter class is responsible for formatting and creating
+    # GitHub Action Check Runs. It extends the Pronto::Formatter::Base class
+    # and integrates with GitHub Actions to generate check runs based
+    # on the messages produced by Pronto runners.
+    #
+    # This class handles the interaction with the GitHub API to create check runs for each
+    # runner, providing detailed annotations and summaries of the analysis results.
+    #
+    # @attr_accessor [Array<Pronto::Message>] messages
+    #   The collection of messages that need to be formatted into check runs.
+    #
+    # @attr_accessor [Pronto::Git::Repository] repo
+    #   The repository object representing the Git repository being analyzed.
+    #
+    # @attr_accessor [String] sha
+    #   The commit SHA for which the check runs are being created. This is typically
+    #   the SHA of the head commit in the repository.
+    #
+    # @attr_accessor [Object] check_run
+    #   The check run object that represents the current check run being processed.
+    #   This object is used to store and manage the state of the check run.
     class GithubActionCheckRunFormatter < Base
       attr_accessor :messages, :repo, :sha, :check_run
 
+      # Formats the messages into GitHub Action Check Runs.
+      #
+      # @param messages [Array<Pronto::Message>] The collection of messages to be formatted.
+      # @param repo [Pronto::Git::Repository] The repository being analyzed.
+      # @return [String] A summary of the number of check runs created.
       def format(messages, repo, _)
         self.messages = messages
         self.repo = repo
@@ -18,6 +44,7 @@ module Pronto
         "#{Runner.runners.size} Check Runs created"
       end
 
+      # @return [Octokit::Client] The Octokit client configured with GitHub API credentials.
       def client
         @client ||= Octokit::Client.new(
           api_endpoint: config.github_api_endpoint,
@@ -26,6 +53,11 @@ module Pronto
         )
       end
 
+      # Creates a GitHub Action Check Run for a given runner and its messages.
+      #
+      # @param runner [Object] The runner for which the check run is being created.
+      # @param runner_messages [Array<Pronto::Message>] The messages associated with the runner.
+      # @return [void]
       def create_check_run(runner, runner_messages)
         line_annotations, no_line_annotations = runner_messages.map do |message|
           Annotation.new(message)
@@ -33,7 +65,7 @@ module Pronto
         output = OpenStruct.new(
           title: runner.title,
           summary: check_run_summary(runner, runner_messages),
-          annotations: line_annotations.map(&:to_h).first(50),
+          annotations: line_annotations.map(&:to_h).first(50)
         )
         if no_line_annotations.any?
           output.text = <<~TXT
@@ -55,6 +87,11 @@ module Pronto
         )
       end
 
+      # Generates a summary for a GitHub Action Check Run.
+      #
+      # @param runner [Object] The runner for which the summary is being generated.
+      # @param runner_messages [Array<Pronto::Message>] The messages associated with the runner.
+      # @return [String] A summary of the check run, indicating the number of issues or a success message.
       def check_run_summary(runner, runner_messages)
         if runner_messages.any?
           <<~TXT
@@ -66,6 +103,10 @@ module Pronto
         end
       end
 
+      # Retrieves the repository slug from the GitHub event or configuration.
+      #
+      # @return [String] The full name of the repository in the format "owner/repo".
+      # @raise [RuntimeError] If the GitHub event path is not set and the slug is not found in the config.
       def repo_slug
         @repo_slug ||= if ENV.key?('GITHUB_EVENT_PATH')
                          event = JSON.parse(File.read(ENV.fetch('GITHUB_EVENT_PATH')))
